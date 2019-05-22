47cf514b941b98a3a2c80e8940992afeeb8182f3
alexander.berkovich <toalexsmail@yahoo.com>
2019-05-22 18:28:33 +0300

    setup.py changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 from alexber.utils import UploadCommand
 
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 NAME = 'python_package_sync_tool'
 SHORT_NAME = 'reqsync'
 VCS_URL = 'https://github.com/alex-ber/PythonPackageSyncTool'
