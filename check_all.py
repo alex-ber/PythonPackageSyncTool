07b970dd7cb752cb7dfebc9baa06ad0210dfe9c1
alex-ber <toalexsmail@yahoo.com>
2021-05-30 13:08:50 +0300

    undo

--- check_all.py
+++ check_all.py
@@ -7,7 +7,6 @@ def main():
 if __name__ == "__main__":
     main()
 
-
 #docker exec -it $(docker ps -q -n=1) bash
 #/etc/unlock_keyring.sh
 #python -m keyring set https://upload.pypi.org/legacy/ alex-ber
